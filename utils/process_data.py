18d5991a2b7e9487145c9d96ad8f01f95aea88a7
Shyam Venkatasubramanian <shyam.venkatasubramanian@gmail.com>
2023-10-13 15:24:29 -0400

    Add files via upload

--- utils/process_data.py
+++ utils/process_data.py
@@ -1,91 +1,165 @@
 import torch
-from itertools import combinations
 import numpy as np
 from sklearn.model_selection import train_test_split
 
 def balanced_batch_generator_reg(data, labels, N, K):
+    """
+    Generates K batches of data and associated labels, each of size N, ensuring 
+    that each data point appears in at least one batch.
+
+    Parameters:
+    - data (numpy.array): The dataset.
+    - labels (numpy.array): Corresponding labels for the dataset.
+    - N (int): Batch size.
+    - K (int): Number of batches.
+
+    Yields:
+    - tuple: Batches of data and corresponding labels.
+    """
+
+    # Determine the total number of samples in the dataset
     num_samples = len(data)
+
+    # Use a set to keep track of batches to ensure uniqueness
     selected_batches = set()
+
+    # All available indices from 0 to num_samples
     all_indices = np.arange(num_samples)
 
-    # Make sure each data point is in at least one batch
+    # Shuffle the indices to ensure randomness
     np.random.shuffle(all_indices)
+
+    # Assign data points to batches, ensuring each data point is in at least one batch
     for i in range(0, num_samples, N):
         selected_batches.add(tuple(sorted(all_indices[i:i+N])))
 
-    # Add additional random unique batches until we have K batches
+    # Continuously sample random unique batches until we have a total of K batches
     while len(selected_batches) < K:
+        # Randomly select N indices without replacement
         batch_indices = tuple(sorted(np.random.choice(num_samples, N, replace=False)))
+
+        # Ensure uniqueness of the batch
         if batch_indices not in selected_batches:
             selected_batches.add(batch_indices)
 
-    selected_batches = list(selected_batches)  # convert back to list for indexing
+    # Convert the set back to a list for easier indexing
+    selected_batches = list(selected_batches)
+
+    # Yield batches of data and their corresponding labels one by one
     for indices in selected_batches:
         yield data[np.array(indices)], labels[np.array(indices)]
 
-def balanced_batch_generator_auto(data, N, K):
+
+def balanced_batch_generator_auto(data, M, K):
+    """
+    Generates K batches of data, each of size M, ensuring that each data point 
+    appears in at least one batch.
+
+    Parameters:
+    - data (torch.Tensor): The dataset.
+    - M (int): Batch size.
+    - K (int): Number of batches.
+
+    Yields:
+    - torch.Tensor: Batches of data.
+    """
+
+    # Determine the total number of samples in the dataset
     num_samples = len(data)
+
+    # Use a set to keep track of batches to ensure uniqueness
     selected_batches = set()
+
+    # All available indices from 0 to num_samples
     all_indices = np.arange(num_samples)
 
-    # Make sure each data point is in at least one batch
+    # Shuffle the indices to ensure randomness
     np.random.shuffle(all_indices)
-    for i in range(0, num_samples, N):
-        selected_batches.add(tuple(sorted(all_indices[i:i+N])))
 
-    # Add additional random unique batches until we have K batches
+    # Assign data points to batches, ensuring each data point is in at least one batch
+    for i in range(0, num_samples, M):
+        selected_batches.add(tuple(sorted(all_indices[i:i+M])))
+
+    # Continuously sample random unique batches until we have a total of K batches
     while len(selected_batches) < K:
-        batch_indices = tuple(sorted(np.random.choice(num_samples, N, replace=False)))
+        # Randomly select N indices without replacement
+        batch_indices = tuple(sorted(np.random.choice(num_samples, M, replace=False)))
+
+        # Ensure uniqueness of the batch
         if batch_indices not in selected_batches:
             selected_batches.add(batch_indices)
 
-    selected_batches = list(selected_batches)  # convert back to list for indexing
-    for indices in selected_batches:
-        yield data[torch.tensor(indices)]  # use torch tensors for indexing
+    # Convert the set back to a list for easier indexing
+    selected_batches = list(selected_batches)
 
-# splitting datasets for regression tasks
+    # Yield batches one by one
+    for indices in selected_batches:
+        yield data[torch.tensor(indices)]  # Convert indices to torch tensor for indexing
+
+
 def SplitDataset(X, y, shift, train_size):
+    """
+    Split the dataset based on the specified shift criterion for regression tasks.
+
+    Parameters:
+    - X (numpy array): Input features.
+    - y (numpy array): Target variable.
+    - shift (float): Determines the split criterion. If -1, a fixed split is applied.
+                     If between 0 and 1, splits based on deviation from mean criterion.
+    - train_size (float): Fraction of data to be used for training in fixed split.
+
+    Returns:
+    - tuple: X_train, X_test, y_train, y_test arrays.
+    """
     
+    # If shift is -1, use a fixed size split
     if shift == -1:
         X_train, X_test, y_train, y_test = train_test_split_fixed(X, y, train_size)
         
-    elif shift >= 0 and shift <= 1:
-        # Assuming X and y are already defined and have the same number of samples
+    # If shift is between 0 and 1, split based on deviation from the mean criterion
+    elif shift > 0 and shift < 1:
+        # Determine number of samples in the dataset
         num_samples = X.shape[0]
         
+        # Compute the mean and standard deviation for the features
         mean_X = np.mean(X, axis=0)
         std_X = np.std(X, axis=0)
         
-        # Compute the condition for each sample in X
+        # Define the condition for deviation from the mean
         condition = np.abs(X - mean_X) < 0.5 * std_X
         
-        # Based on the condition, sample the indices for the training set
+        # Initialize list to store training data indices
         train_indices = []
         
+        # For each sample, check the condition and decide if it goes into the training set
         for i in range(num_samples):
+            # If all features of a sample satisfy the condition
             if condition[i].all():
-                # Sample with probability p
+                # Sample the index with probability 'shift'
                 if np.random.rand() < shift:
                     train_indices.append(i)
             else:
-                # Sample with probability 1-p
+                # For samples not meeting the condition, sample with probability '1-shift'
                 if np.random.rand() < 1 - shift:
                     train_indices.append(i)
 
-        # All other indices are for the test set
+        # Indices not in train_indices form the test set
         test_indices = [i for i in range(num_samples) if i not in train_indices]
         
+        # Split the data into training and test sets based on selected indices
         X_train = X[train_indices]
         y_train = y[train_indices]
         
         X_test = X[test_indices]
         y_test = y[test_indices]
 
+    # If shift is not between 0 and 1 or -1, raise an error
     else:
         print('Error: Shift must be between 0 and 1')
+        exit(1)
         
     return X_train, X_test, y_train, y_test
-        
+
         
 def train_test_split_fixed(X, y, train_size, shuffle=True, random_state=None):
     """
@@ -106,7 +180,8 @@ def train_test_split_fixed(X, y, train_size, shuffle=True, random_state=None):
     
     # Ensure train_size is valid
     if not (0 <= train_size < X.shape[0]):
-        raise ValueError("train_size must be between 0 and the total number of samples.")
+        print("Error: TrainSize must be between 0 and the total number of samples.")
+        exit(1)
     
     if shuffle:
         np.random.seed(random_state)
@@ -124,3 +199,24 @@ def train_test_split_fixed(X, y, train_size, shuffle=True, random_state=None):
     y_test = y[test_indices]
 
     return X_train, X_test, y_train, y_test
+
+
+def add_gaussian_noise(X_train, alpha):
+    """
+    Add standard normal noise with specified scaling factor (alpha) to X_train.
+    
+    Parameters:
+    - X_train (numpy array): The input data.
+    - alpha (float): The scaling factor.
+    
+    Returns:
+    - numpy array: The noisy data.
+    """
+    
+    # Generate Gaussian noise of the same shape as X_train
+    noise = np.random.normal(0, 1, X_train.shape)
+    
+    # Add the noise to X_train
+    X_train = X_train + noise * alpha
+    
+    return X_train
--- utils/upload_data.py
+++ utils/upload_data.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.datasets import fetch_california_housing
+from sklearn.datasets import fetch_california_housing, fetch_openml
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
@@ -9,24 +9,27 @@ def LoadDataset(DatasetName):
         # load California Housing
         california = fetch_california_housing()
         output1, output2 = california.data, np.expand_dims(california.target,1)
-    elif DatasetName == "Diabetes":
-        pass # load Diabetes dataset
+    elif DatasetName == "WineQuality":
+        # Fetch the "Elevators" dataset from OpenML
+        wine = fetch_openml(name='wine_quality', version=1)
+        output1, output2 = wine.data.to_numpy(), np.expand_dims(wine.target.to_numpy(),1)
     elif DatasetName == "1DPolynomial":
         pass # create dataset
-    elif DatasetName == "5DPolynomial":
+    elif DatasetName == "5DPolynomial": 
         pass # create dataset
     elif DatasetName == "TrigonometricFunctions":
         pass # create dataset
     # image datasets
     elif DatasetName == "MNIST":
         transform = transforms.ToTensor()
-        output1 = datasets.MNIST(root='data/', train=True, transform=transform, download=False)
-        output2 = datasets.MNIST(root='data/', train=False, transform=transform, download=False)
-        print("MNIST dataset loaded")
+        output1 = datasets.MNIST(root='data/', train=True, transform=transform, download=True)
+        output2 = datasets.MNIST(root='data/', train=False, transform=transform, download=True)
     elif DatasetName == "CIFAR10":
         transform = transforms.ToTensor()
         output1 = datasets.CIFAR10(root="./data", train=True, transform=transform, download=True)
         output2 = datasets.CIFAR10(root="./data", train=False, transform=transform, download=True)
     else:
-        print("Error: Dataset name is undefined")      
-    return output1,output2
+        print("Error: Dataset name is undefined")
+        exit(1)
+  
+    return output1, output2
