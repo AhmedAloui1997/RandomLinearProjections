16c240dbe8129ad4360c413d031f545f1c12d42d
Shyam Venkatasubramanian <shyam.venkatasubramanian@gmail.com>
2023-10-14 11:06:04 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ if __name__ == "__main__":
     parser.add_argument('--Iterations', type=int, default=30, help='Number of iterations to repeat task')
     parser.add_argument('--Shift', type=float, default=-1, help="Dataset shift", required=False)
     parser.add_argument('--Noise', type=float, default=-1, help="Noise Scaling Factor", required=False)
+    parser.add_argument('--EvalMetric', type=str, default='MSE', help='Evaluation Metric (MSE or RLP)', required=False)
     args = parser.parse_args()
     
     DatasetName = args.Dataset
@@ -35,12 +36,13 @@ if __name__ == "__main__":
     iterations = args.Iterations
     shift = args.Shift
     noise = args.Noise
+    eval_metric = args.EvalMetric
 
     # This function calls the dataset we need for the experiment
     # Distinguish between regression and autoencoder
     if task == 'Regression':
         X, y = LoadDataset(DatasetName)
-        train_losses, test_losses = train(X, y, noise, shift, train_size, task, iterations, epochs, batch_size, num_batches, loss_function)
+        train_losses, test_losses = train(X, y, eval_metric, noise, shift, train_size, task, iterations, epochs, batch_size, num_batches, loss_function)
     elif task == 'Autoencoder':
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         X_train, X_test = LoadDataset(DatasetName)
@@ -54,14 +56,29 @@ if __name__ == "__main__":
     
     # Path for the directory
     results_dir = 'Results'
+    MSE_dir = 'Results/MSE'
+    RLP_dir = 'Results/RLP'
     
     # Check if the directory exists, and if not, create it
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
+    # Check if the directory exists, and if not, create it
+    if not os.path.exists(MSE_dir):
+        os.makedirs(MSE_dir)
+    # Check if the directory exists, and if not, create it
+    if not os.path.exists(RLP_dir):
+        os.makedirs(RLP_dir)
         
     # Path for the CSV files
-    train_losses_path = f'Results/TRAIN_{DatasetName}_{task}_{loss_function}_TrainSize={train_size}_BatchSize={batch_size}_NumBatches={num_batches}_Epochs={epochs}_Iterations={iterations}_Shift={shift}_Noise={noise}.csv'
-    test_losses_path = f'Results/TEST_{DatasetName}_{task}_{loss_function}_TrainSize={train_size}_BatchSize={batch_size}_NumBatches={num_batches}_Epochs={epochs}_Iterations={iterations}_Shift={shift}_Noise={noise}.csv'
+    if eval_metric == 'MSE': 
+        train_losses_path = f'Results/MSE/TRAIN_{DatasetName}_{task}_{loss_function}_TrainSize={train_size}_BatchSize={batch_size}_NumBatches={num_batches}_Epochs={epochs}_Iterations={iterations}_Shift={shift}_Noise={noise}.csv'
+        test_losses_path = f'Results/MSE/TEST_{DatasetName}_{task}_{loss_function}_TrainSize={train_size}_BatchSize={batch_size}_NumBatches={num_batches}_Epochs={epochs}_Iterations={iterations}_Shift={shift}_Noise={noise}.csv'
+    elif eval_metric == 'RLP':
+        train_losses_path = f'Results/RLP/TRAIN_{DatasetName}_{task}_{loss_function}_TrainSize={train_size}_BatchSize={batch_size}_NumBatches={num_batches}_Epochs={epochs}_Iterations={iterations}_Shift={shift}_Noise={noise}.csv'
+        test_losses_path = f'Results/RLP/TEST_{DatasetName}_{task}_{loss_function}_TrainSize={train_size}_BatchSize={batch_size}_NumBatches={num_batches}_Epochs={epochs}_Iterations={iterations}_Shift={shift}_Noise={noise}.csv'
+    else:
+        print('Error: Evaluation metric must be MSE or RLP')
+        exit(1)
     
     # Save matrices to CSV
     np.savetxt(train_losses_path, train_losses, delimiter=',')
