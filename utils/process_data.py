16b3bbfcecc6d5ee7daa80e7726f078fdb274739
Shyam Venkatasubramanian <shyam.venkatasubramanian@gmail.com>
2023-10-19 13:59:29 -0400

    Add files via upload

--- utils/process_data.py
+++ utils/process_data.py
@@ -210,3 +210,19 @@ def add_gaussian_noise(X_train, alpha):
     X_train = X_train + noise * alpha
     
     return X_train
+
+
+# Define the mixup function
+def mixup_data(x, y, alpha=1.0):
+    '''Returns mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    index = torch.randperm(batch_size)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
